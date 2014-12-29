95baf3daa0cacd587de6c27dd7bb42976e2ff4cc
Piyush <piyush.mishra@sodelsolutions.com>
2014-12-29 13:41:01 +0530

    ArrayLinearSearch in java

---
+++ src/main/java/com/algo/search/ArrayLinearSearchJava.java
@@ -0,0 +1,20 @@
+package com.algo.search;
+
+public class ArrayLinearSearchJava {
+
+	public static int linearSerach(int[] array, int item) {
+		int index = -1;
+		for (int i = 0; i <= array.length - 1; i++) {
+			if (array[i] == item)
+				index = i;
+		}
+		return index;
+	}
+
+	public static void main(String[] args) {
+
+		int data[] = new int[] { 10, 20, 30, 40, 50, 60, 71, 80, 90, 91 };
+
+		System.out.println(linearSerach(data,8954));
+	}
+}
