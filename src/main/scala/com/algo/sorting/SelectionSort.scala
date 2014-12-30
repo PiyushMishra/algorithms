1e1b7fe96032a6b38e5c40e411635b3bc4cd02f8
Piyush <piyush.mishra@sodelsolutions.com>
2014-12-30 15:32:43 +0530

    selection sort in java

--- src/main/scala/com/algo/sorting/SelectionSort.scala
+++ src/main/scala/com/algo/sorting/SelectionSort.scala
@@ -1,9 +1,22 @@
 package com.algo.sorting
 
+/**
+ * @author piyush
+ * The selection sort algorithm sorts an array by repeatedly finding the minimum element (considering ascending order) from
+ * unsorted part and putting it at the beginning. The algorithm maintains two subarrays in a given array.
+ *
+ * 1) The subarray which is already sorted.
+ * 2) Remaining subarray which is unsorted.
+ *
+ * In every iteration of selection sort, the minimum element (considering ascending order) from the unsorted subarray is picked
+ * and moved to the sorted subarray.
+ *
+ * Following example explains the above steps:
+ */
+
 object SelectionSort extends App {
-
-  val array = Seq(24, 67, 89, 24, 5, 7, 456, 789, 34, 98)
-
+  val intmin = Int.MinValue
+  val array = Seq(24, 67, 89, 24, 5, 7, 456, 789, 34, 98, -1)
   /**
    * @param array
    * @return
@@ -11,7 +24,7 @@ object SelectionSort extends App {
    */
   def min(array: Seq[Int], acc: Seq[Int]): (Int, Seq[Int]) = {
     array match {
-      case Nil => (-1, acc)
+      case Nil => (intmin, acc)
       case x :: Nil => (x, acc)
       case x :: xs => if (x > xs.head) min(xs, acc.:+(x)) else min(x :: xs.tail, acc.:+(xs.head))
     }
@@ -19,7 +32,7 @@ object SelectionSort extends App {
 
   def sort(array: Seq[Int], acc: Seq[Int]): Seq[Int] = {
     min(array, Seq()) match {
-      case (-1, _) => acc
+      case (`intmin`, _) => acc
       case (x, xs) => sort(xs, acc.:+(x))
     }
   }
