9d9537b24b548cebf160434a5e922389fc5cab87
Piyush <piyush.mishra@sodelsolutions.com>
2014-12-31 18:22:59 +0530

    merge sort in scala

--- src/main/scala/com/algo/sorting/MergeSort.scala
+++ src/main/scala/com/algo/sorting/MergeSort.scala
@@ -4,20 +4,20 @@ object MergeSort extends App {
   val numbers = Seq(24, 67, 89, 24, 5, 7, 456, 789, 34, 98, -1)
 
   def merge(left: Seq[Int], right: Seq[Int]) = {
-    if (left.size > right.size) sort(left, right, Seq()) else sort(right, left, Seq())
+    if (left.size >= right.size) sort(left, right, Seq()) else sort(right, left, Seq())
   }
 
   def sort(left: Seq[Int], right: Seq[Int], result: Seq[Int]): Seq[Int] = {
     (left, right) match {
       case (xs, Nil) => result ++ xs
-      case (xs, ys) => 
-        if (ys(0) < xs(0)) sort(xs, ys.tail, result.:+(ys(0))) else sort(xs.tail, ys.tail, result.:+(xs(0)))
+      case (Nil, ys) => result ++ ys
+      case (xs, ys) =>  if (ys(0) < xs(0)) sort(xs, ys.tail, result.:+(ys(0))) else sort(xs.tail, ys, result.:+(xs(0)))
     }
   }
 
   def mergeSort(numbers: Seq[Int]): Seq[Int] = {
     numbers.length match {
-      case 0|1 => numbers
+      case 1 => numbers
       case x: Int =>
         val middle = numbers.length / 2
         val (leftHalf, rightHalf) = numbers.splitAt(middle)
