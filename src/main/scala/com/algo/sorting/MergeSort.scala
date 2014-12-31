16766be0eaec2f8453f936a0838a6b2bb69f421f
Piyush <piyush.mishra@sodelsolutions.com>
2014-12-31 18:35:59 +0530

    refactored merge sort in scala

--- src/main/scala/com/algo/sorting/MergeSort.scala
+++ src/main/scala/com/algo/sorting/MergeSort.scala
@@ -1,18 +1,20 @@
 package com.algo.sorting
 
+import scala.annotation.tailrec
+
 object MergeSort extends App {
   val numbers = Seq(24, 67, 89, 24, 5, 7, 456, 789, 34, 98, -1)
 
   def merge(left: Seq[Int], right: Seq[Int]) = {
-    if (left.size >= right.size) sort(left, right, Seq()) else sort(right, left, Seq())
-  }
-
-  def sort(left: Seq[Int], right: Seq[Int], result: Seq[Int]): Seq[Int] = {
-    (left, right) match {
-      case (xs, Nil) => result ++ xs
-      case (Nil, ys) => result ++ ys
-      case (xs, ys) =>  if (ys(0) < xs(0)) sort(xs, ys.tail, result.:+(ys(0))) else sort(xs.tail, ys, result.:+(xs(0)))
+    @tailrec
+    def sort(left: Seq[Int], right: Seq[Int], result: Seq[Int]): Seq[Int] = {
+      (left, right) match {
+        case (xs, Nil) => result ++ xs
+        case (Nil, ys) => result ++ ys
+        case (xs, ys) => if (ys(0) < xs(0)) sort(xs, ys.tail, result.:+(ys(0))) else sort(xs.tail, ys, result.:+(xs(0)))
+      }
     }
+    sort(left, right, Seq())
   }
 
   def mergeSort(numbers: Seq[Int]): Seq[Int] = {
